7da4c686c75a502ce1e30a3fe8be19ecd941042c
diogob89 <diogob1789@gmail.com>
2016-10-01 04:36:37 -0300

    Add files via upload

---
+++ site gremio/header.php
@@ -0,0 +1,16 @@
+<header>
+	
+	<div class="centro" style="position:relative;">
+	<div class="animacao"></div> 
+		<div id="logo"><a href="home.html"><img src ="logo.jpg" class="tamanhologo" alt="Grêmio logo"></a></div>
+		<div class ="titulo"><h1> Grêmio USA </h1></div>
+		
+			<div class = "menusocial">
+			<a href="https://www.facebook.com/GREMIOUSA/?fref=ts" target="_blank"><img id="facebook" src ="facebook.png" alt="Facebook botão" style="height:35px"></a>
+			<a href="https://twitter.com/gremio_usa" target="_blank"><img id="twitter" src ="twitter.png" alt="Twitter botão" style="height:35px"></a>
+			</div>
+			
+	</div>
+	
+	
+	</header>
